1511363edb05596a44d55d455984812407fd4ef3
RASC <rasc@sapo.pt>
2024-06-22 22:53:33 +0200

    removed the console log

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,5 @@
 const setYearFooter = () => {
     const yearSpan = document.getElementById("year")
-    console.log(yearSpan)
     const date = new Date()
 
     yearSpan.innerText = date.getFullYear()
